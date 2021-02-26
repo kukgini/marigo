5d4d0855971e17715beb9a57d3aa55bf6066a6ba
kukgini <kukgini@gmail.com>
2021-02-26 19:25:13 +0900

    ...

--- main.go
+++ main.go
@@ -2,36 +2,34 @@ package main
 
 import (
 	"log"
-/*
-	"github.com/hyperledger/aries-framework-go-ext/component/vdr/indy"
-	"github.com/hyperledger/indy-vdr/wrappers/golang/vdr"
-*/
+	/*
+		"github.com/hyperledger/aries-framework-go-ext/component/vdr/indy"
+		"github.com/hyperledger/indy-vdr/wrappers/golang/vdr"
+	*/
 	"github.com/hyperledger/aries-framework-go/pkg/client/didexchange"
 	"github.com/hyperledger/aries-framework-go/pkg/framework/aries"
 	"github.com/hyperledger/aries-framework-go/pkg/vdr/httpbinding"
 )
 
 func main() {
-    log.Println("create httpbinding vdr")
-    vdr, err := httpbinding.New("http://127.0.0.1:3000/did")
-    if err != nil {
-        log.Fatalln(err)
-    }
-    log.Println("check vdr is available")
-    did, err := vdr.Read("21tDAKCERh95uGgKbJNHYp.jsonld")
-    //did, err := vdr.Read("123456789abcdefghi.jsonld")
-    //did, err := vdr.Read("simple-example.jsonld")
-    if err != nil {
-        log.Println("read did failed")
-        log.Fatalln(err)
-    }
-    log.Println(did)
+	log.Println("create httpbinding vdr")
+	vdr, err := httpbinding.New("http://127.0.0.1:3000/did")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	log.Println("check vdr is available")
+	did, err := vdr.Read("21tDAKCERh95uGgKbJNHYp")
+	if err != nil {
+		log.Println("read did failed")
+		log.Fatalln(err)
+	}
+	log.Println(did)
 
 	log.Println("create aries framework")
 	framework, err := aries.New(aries.WithVDR(vdr))
-    if err != nil {
-        log.Fatalln(err)
-    }
+	if err != nil {
+		log.Fatalln(err)
+	}
 
 	log.Println("get current aries context")
 	ctx, _ := framework.Context()
@@ -48,9 +46,27 @@ func main() {
 	             - did: %s
 	             - type: %s
                `
-    log.Printf(resout, 
-        invitation.Invitation.ID, 
-        invitation.Invitation.Label, 
-        invitation.Invitation.DID, 
-        invitation.Invitation.Type)
+	log.Printf(resout,
+		invitation.Invitation.ID,
+		invitation.Invitation.Label,
+		invitation.Invitation.DID,
+		invitation.Invitation.Type)
+
+	log.Println("create invitation with DID")
+	invitation2, err := client.CreateInvitationWithDID("invitationPublic", "21tDAKCERh95uGgKbJNHYp")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resout2 := `created invitation:
+	             - id: %s 
+	             - label: %s
+	             - did: %s
+	             - type: %s
+               `
+	log.Printf(resout2,
+		invitation2.Invitation.ID,
+		invitation2.Invitation.Label,
+		invitation2.Invitation.DID,
+		invitation2.Invitation.Type)
 }
