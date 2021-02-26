b1247d2e6d78d954a06b0ecbaeba17b3045f8ae5
kukgini <kukgini@gmail.com>
2021-02-26 17:26:44 +0900

    ...

--- go.mod
+++ go.mod
@@ -5,8 +5,8 @@ go 1.15
 require (
 	github.com/hyperledger/aries-framework-go v0.1.6-0.20201230175227-2b6c7532c011
 	github.com/hyperledger/aries-framework-go-ext v0.0.0-20210209170459-14c492334960 // indirect
-	github.com/hyperledger/aries-framework-go-ext/component/vdr/indy v0.0.0-20210209170459-14c492334960
-	github.com/hyperledger/indy-vdr/wrappers/golang v0.0.0-20201031155907-5f437d26ed71
+	github.com/hyperledger/aries-framework-go-ext/component/vdr/indy v0.0.0-20210209170459-14c492334960 // indirect
+	github.com/hyperledger/indy-vdr/wrappers/golang v0.0.0-20201031155907-5f437d26ed71 // indirect
 	github.com/jaytaylor/mockery-example v0.0.0-20170323165341-bf04a9147d8e // indirect
 	github.com/onsi/ginkgo v1.10.3 // indirect
 	github.com/onsi/gomega v1.7.1 // indirect
--- main.go
+++ main.go
@@ -1,53 +1,54 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net/http"
-
+/*
 	"github.com/hyperledger/aries-framework-go-ext/component/vdr/indy"
+	"github.com/hyperledger/indy-vdr/wrappers/golang/vdr"
+*/
 	"github.com/hyperledger/aries-framework-go/pkg/client/didexchange"
 	"github.com/hyperledger/aries-framework-go/pkg/framework/aries"
-	"github.com/hyperledger/indy-vdr/wrappers/golang/vdr"
+	"github.com/hyperledger/aries-framework-go/pkg/vdr/httpbinding"
 )
 
 func main() {
-	log.Println("load genesis tx from internet")
-	genesisFile, err := http.Get("https://raw.githubusercontent.com/sovrin-foundation/sovrin/master/sovrin/pool_transactions_builder_genesis")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer genesisFile.Body.Close()
+    log.Println("create httpbinding vdr")
+    vdr, err := httpbinding.New("http://127.0.0.1:8000/nym")
+    if err != nil {
+        log.Fatalln(err)
+    }
+    log.Println("check vdr is available")
+    did, err := vdr.Read("2wJPyULfLLnYTEFYzByfUR")
+    if err != nil {
+        log.Println("read did failed")
+        log.Fatalln(err)
+    }
+    log.Println(did)
 
-	fmt.Println("create indy vdr client")
-	indyClient, err := vdr.New(genesisFile.Body)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Fatalln(indyClient)
+	log.Println("create aries framework")
+	framework, err := aries.New(aries.WithVDR(vdr))
+    if err != nil {
+        log.Fatalln(err)
+    }
 
-	fmt.Println("create indy vdr client")
-	client, err := indy.New("sov", indy.WithIndyClient(indyClient))
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Fatalln(indyClient)
-
-	fmt.Println("create aries framework")
-	framework, _ := aries.New()
-
-	fmt.Println("get current aries context")
+	log.Println("get current aries context")
 	ctx, _ := framework.Context()
 
-	fmt.Println("create new didexchange client")
+	log.Println("create new didexchange client")
 	client, _ := didexchange.New(ctx)
 
-	fmt.Println("create invitation")
+	log.Println("create invitation")
 	invitation, _ := client.CreateInvitation("test")
 
-	fmt.Println("invitation created:")
-	fmt.Println(invitation.Invitation.ID)
-	fmt.Println(invitation.Invitation.Label)
-	fmt.Println(invitation.Invitation.DID)
-	fmt.Println(invitation.Invitation.Type)
+	resout := `created invitation:
+	             - id: %s 
+	             - label: %s
+	             - did: %s
+	             - type: %s
+               `
+    log.Printf(resout, 
+        invitation.Invitation.ID, 
+        invitation.Invitation.Label, 
+        invitation.Invitation.DID, 
+        invitation.Invitation.Type)
 }
